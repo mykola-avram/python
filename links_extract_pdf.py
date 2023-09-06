93467623ddc4b1b8278a0cf9eb50bf91afc7f233
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2023-09-06 21:21:36 +0200

    Update links_extract_pdf.py

--- links_extract_pdf.py
+++ links_extract_pdf.py
@@ -4,6 +4,10 @@ import re
 pdf_file = open("C:/Users/mavram/Downloads/Power BI Training Instruction_Edited.pdf", 'rb')
 import PyPDF2
 
+import PyPDF2
+import requests
+from tabulate import tabulate
+
 def extract_links(page):
     links = []
     if '/Annots' in page:
@@ -18,6 +22,17 @@ def extract_links(page):
 # Create a PDF reader object
 pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 
+# Function to check the validity of a URL by making a GET request
+def is_url_valid(url):
+    try:
+        response = requests.get(url)
+        return response.status_code == 200
+    except requests.ConnectionError:
+        return False
+
+# Initialize a list to store the results
+results = []
+
 # Loop through each page in the PDF
 for page_num in range(pdf_reader.getNumPages()):
     page = pdf_reader.getPage(page_num)
@@ -26,6 +41,17 @@ for page_num in range(pdf_reader.getNumPages()):
     if page_links:
         for link in page_links:
             print(f'Page {page_num + 1}: {link}')
+            if is_url_valid(link):
+                print('  This link is valid and exists on the internet.')
+            else:
+                print('  This link is invalid or does not exist on the internet.')
+
+            link_status = "Valid" if is_url_valid(link) else "Invalid"
+            results.append([f'Page {page_num + 1}', link, link_status])
 
 # Close the PDF file
 pdf_file.close()
+
+# Display the results as a table
+table_headers = ["Page Number", "Link", "Status"]
+print(tabulate(results, headers=table_headers, tablefmt="pretty"))
