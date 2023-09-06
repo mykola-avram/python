f3396f72bdbed6727d5d5e92d48dfde0c467061d
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2023-09-06 21:44:31 +0200

    Update links_extract_pdf.py

--- links_extract_pdf.py
+++ links_extract_pdf.py
@@ -1,12 +1,12 @@
 import pikepdf 
 import PyPDF2
 import re
-pdf_file = open("C:/Users/mavram/Downloads/Power BI Training Instruction_Edited.pdf", 'rb')
+pdf_file = open("file.pdf", 'rb')
 import PyPDF2
 
 import PyPDF2
 import requests
-from tabulate import tabulate
+from selenium import webdriver
 
 def extract_links(page):
     links = []
@@ -19,19 +19,28 @@ def extract_links(page):
                 links.append(uri)
     return links
 
+# Function to check the validity of a URL using Selenium
+def is_url_valid_with_selenium(url):
+    try:
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        
+        driver.get(url)
+        
+        # Check if the page loaded successfully (you can modify this check as needed)
+        if "Amazon.com" in driver.title:
+            return True
+        
+        return False
+    except Exception as e:
+        print(e)
+        return False
+    finally:
+        driver.quit()
+
 # Create a PDF reader object
 pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-
-# Function to check the validity of a URL by making a GET request
-def is_url_valid(url):
-    try:
-        response = requests.get(url)
-        return response.status_code == 200
-    except requests.ConnectionError:
-        return False
-
-# Initialize a list to store the results
-results = []
 
 # Loop through each page in the PDF
 for page_num in range(pdf_reader.getNumPages()):
@@ -41,17 +50,18 @@ for page_num in range(pdf_reader.getNumPages()):
     if page_links:
         for link in page_links:
             print(f'Page {page_num + 1}: {link}')
-            if is_url_valid(link):
-                print('  This link is valid and exists on the internet.')
+            if "amazon.com" in link.lower():
+                is_valid = is_url_valid_with_selenium(link)
+                if is_valid:
+                    print('  This link is valid and exists on the internet.')
+                else:
+                    print('  This link is invalid or does not exist on the internet (Amazon page).')
             else:
-                print('  This link is invalid or does not exist on the internet.')
-
-            link_status = "Valid" if is_url_valid(link) else "Invalid"
-            results.append([f'Page {page_num + 1}', link, link_status])
+                if requests.get(link).status_code == 200:
+                    print('  This link is valid and exists on the internet.')
+                else:
+                    print('  This link is invalid or does not exist on the internet.')
 
 # Close the PDF file
 pdf_file.close()
 
-# Display the results as a table
-table_headers = ["Page Number", "Link", "Status"]
-print(tabulate(results, headers=table_headers, tablefmt="pretty"))
